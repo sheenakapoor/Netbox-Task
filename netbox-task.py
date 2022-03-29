cec189bdd94ae92e8a62b1e27dc9c3018fc2df6a
Sheena Kapoor <sheenak@andrew.cmu.edu>
2022-03-29 07:20:11 +0000

    updated code with comments

--- netbox-task.py
+++ netbox-task.py
@@ -1,81 +1,69 @@
-# taking input from the user for their Zip Code and displaying it
+# Taking input from user for Zip Code, displaying it
 zipcode = input("User's US Post Office Zip Code: ")
-print(zipcode)
+print(f'Zip Code: {zipcode} \n')
 
-# importing libraries
+# Importing Libraries
 import requests
 
-# generating url based on user's zip code
+# Generating URL based on User's Postal Zip Code
 url = "http://wttr.in/" + zipcode
 
-# getting JSON weather report
+# Converting to JSON
 params = (('format', 'j1'),)
 response = requests.get(url, params=params).json()
-# printing json output
-# print(response.json())
 
-# getting the current temp, feels like temp, and weather description for user's zip code
+# Getting the Current Temperature, Feels-Like Temperature, and Weather Description for user's Zip Code
 
-# generating different params for all above mentioned
-params1 = (('format', '"Current Temperature: %t"'),)
-r_t = requests.get(url, params=params1).json()
-params2 = (('format', '"Feels like Temperature: %f"'),)
-r_f = requests.get(url, params=params2).json()
-params3 = (('format', '"Weather Description: %C"'),)
-r_C = requests.get(url, params=params3).json()
+# The current condition key contains required information hence assigning variable and indexing it
+current_condition = response['current_condition'][0]
 
-print('The weather conditions in your location are displayed below:',"\n",r_t,"\n",r_f,"\n",r_C)
+# Assigning variables and indexing to get Current Temperature, Feels-Like Temperature and Weather Description for user's Zip Code
+temp_F = current_condition['temp_F']
+FeelsLikeF = current_condition['FeelsLikeF']
+weatherDesc = current_condition['weatherDesc'][0]['value']
 
+# Displaying weather conditions with temperatures in Fahrenheit
+print(f"The weather conditions in your location are displayed below: \n Current Temperature: {temp_F}°F \n Feels like Temperature: {FeelsLikeF}°F \n Weather Description: {weatherDesc} \n")
 
-# Conversion of current temperature from F to C.
+# Conversion of Current Temperature and Feels-Like Temperature from F to C respectively.
 
-# retrieving current temperature 
-t_param = (('format', '"%t"'),)
-current_temp = requests.get(url, params=t_param).json()
-# slicing to get sign and digits of temperature, converting string to float
-F_current = float(current_temp[0:3])
+# Changing type from str to float for Math Operations
+temp_F_float = float(temp_F)
+FeelsLikeF_float = float(FeelsLikeF)
 
-# converting fahrenheit to celcius and rounding it to one decimal
-C_current = round(((5/9) * (F_current-32)), 1)
-print(f'The Actual Temperature in Celcius is {C_current}°C')
+# Defining a function to convert units from Fahrenheit to Celcius
+def FtoC(F):
+    C = (5/9) * (F-32)
+    # Rounding output to one decimal place
+    C_rounded = round(C, 1)
+    return C_rounded
 
-
-# Conversion of feels-like temperature now from F to C.
-
-# retrieving feels like temp now
-feel_t_param = (('format', '"%f"'),)
-feels_like_temp = requests.get(url, params=feel_t_param).json()
-# slicing to get sign and digits of temperature, converting string to float
-F_feels_like = float(feels_like_temp[0:3])
-
-# converting fahrenheit to celcius and rounding it to one decimal
-C_feels_like = round(((5/9) * (F_feels_like-32)),1)
-print(f'The Feels-Like Temperature in Celcius is {C_feels_like}°C')
+# Displaying Temperatures in Celcius from the defined function FtoC
+print(f'The Current Temperature in Celcius is {FtoC(temp_F_float)}°C')
+print(f'The Feels-Like Temperature in Celcius is {FtoC(FeelsLikeF_float)}°C \n')
 
 # BONUS TASK
 
-# emojis for ranges of temp 
-num = C_current
-# custom ranges (4 different ranges)
+#Initializing variable for the current temperature obtained in Celcius
+num = FtoC(temp_F_float)
+
+# Displaying Emojis for Custom Ranges (4 different ranges) of Current Temperature (in Celcius)
 if num <= 0:
-    print(f"Today's weather in your location: \u2744\uFE0F, {num}°C")
+    print(f"Today's weather in your location: \u2744\uFE0F {num}°C \n")
 elif 0.1 <= num <= 15:
-    print(f"Today's weather in your location: \u2601\uFE0F, {num}°C")
+    print(f"Today's weather in your location: \u2601\uFE0F {num}°C \n")
 elif 15.1 <= num <= 30:
-    print(f"Today's weather in your location: \u26C5, {num}°C")
+    print(f"Today's weather in your location: \u26C5 {num}°C \n")
 elif num > 30:
-    print(f"Today's weather in your location:\u2600\uFE0F, {num}°C")
+    print(f"Today's weather in your location:\u2600\uFE0F {num}°C \n")
 
 # BONUS BONUS TASK
 
-print(f'Weather forecast for three hours from now is as follows:')
-
-# we know that the json output has weather forecast data at a 3 hour time step
-# retrieving data for 1 time step from now (at index 1)
+# Since the JSON output has weather forecast data at a 3 hour time step, retrieving data for 1 time step from now at index 1:
 three_hours = response['weather'][0]['hourly'][1]
 
-#flt = feels like temperature, ct = current temperature, wd = weather description
+#flt = feels like temperature, at = actual temperature, wd = weather description
+next_at = three_hours["tempF"]
 next_flt = three_hours["FeelsLikeF"]
-next_ct = three_hours["tempF"]
 next_wd = three_hours["weatherDesc"][0]["value"]
-print(f'\n Feels like Temperature: {next_flt}°F \n Current Temperature: {next_ct}°F \n Weather Description: {next_wd}')
+print(f'Weather forecast for three hours from now is as follows: \n Actual Temperature: {next_at}°F \n Feels like Temperature: {next_flt}°F \n Weather Description: {next_wd} \n')
